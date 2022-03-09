8df3f20d224d2a31bcd12185a339a7d248437e25
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-03-09 21:54:48 +0100

    Add files via upload

---
+++ Variables PHP/test.php
@@ -0,0 +1,42 @@
+<?php
+$movie = "Indiana Jones and the Last crusade";
+$userWatchers = true;
+$releaseDate = "05.24.1989";
+$rating = 8.3/10;
+
+echo $movie .' '. $releaseDate;
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
